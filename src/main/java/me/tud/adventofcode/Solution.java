d2209f900cf73ccabcc2c1b49a54e518dffb3218
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2023-12-10 13:45:32 +0300

    Remove unused import

--- src/main/java/me/tud/adventofcode/Solution.java
+++ src/main/java/me/tud/adventofcode/Solution.java
@@ -1,7 +1,6 @@
 package me.tud.adventofcode;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Objects;
