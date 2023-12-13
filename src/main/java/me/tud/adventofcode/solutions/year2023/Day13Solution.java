d11b5f65319ebe5ee08abef2f37b75952aef10e5
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2023-12-13 20:59:36 +0300

    Day 13 solution

--- src/main/java/me/tud/adventofcode/solutions/year2023/Day13Solution.java
+++ src/main/java/me/tud/adventofcode/solutions/year2023/Day13Solution.java
@@ -4,49 +4,57 @@ import me.tud.adventofcode.AdventOfCodeSolution;
 import me.tud.adventofcode.Solution;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @AdventOfCodeSolution(year = 2023, day = 13, name = "Point of Incidence", link = "https://adventofcode.com/2023/day/13")
 public class Day13Solution extends Solution {
 
     @Override
     public Long part1Solution() {
-        long columns = 0, rows = 0;
-        String[] data = getInputLines();
-        for (int from = data.length - 1, to = from + 1; from >= 0; from--) {
-            if (from != 0 && !data[from - 1].isEmpty()) continue;
-            String[] subArray = Arrays.copyOfRange(data, from, to);
-            columns += findVerticalReflection(subArray);
-            rows += findHorizontalReflection(subArray);
-            to = from - 1;
-        }
-        return columns + rows * 100;
+        return solve(0);
     }
 
     @Override
     public Long part2Solution() {
-        return null;
+        return solve(1);
     }
 
-    private int findVerticalReflection(String[] data) {
+    private long solve(int allowedSmudges) {
+        long columns = 0, rows = 0;
+        String[] data = getInputLines();
+        for (int from = 0, to = 0; to < data.length; to++) {
+            if (data.length - to != 1 && !data[to].isEmpty()) continue;
+            String[] layers = Arrays.copyOfRange(data, from, to);
+            columns += findVerticalReflection(layers, allowedSmudges);
+            rows += findHorizontalReflection(layers, allowedSmudges);
+            from = to + 1;
+        }
+        return columns + rows * 100;
+    }
+
+    private int findVerticalReflection(String[] layers, int smudges) {
         outer:
-        for (int i = 1; i < data[0].length(); i++) {
-            for (String line : data) {
-                if (!isMirrored(line, i))
-                    continue outer;
+        for (int i = 1; i < layers[0].length(); i++) {
+            int totalSmudges = 0;
+            for (String line : layers) {
+                totalSmudges += isMirrored(line, i);
+                if (totalSmudges > smudges) continue outer;
             }
-            return i;
+            if (totalSmudges == smudges) return i;
         }
         return 0;
     }
 
-    private int findHorizontalReflection(String[] data) {
+    private int findHorizontalReflection(String[] layers, int smudges) {
         outer:
-        for (int i = 1; i < data.length; i++) {
-            for (int j = 0; j < data[0].length(); j++) {
-                if (!isMirrored(getVerticalLine(data, j), i))
-                    continue outer;
+        for (int i = 1; i < layers.length; i++) {
+            int totalSmudges = 0;
+            for (int j = 0; j < layers[0].length(); j++) {
+                totalSmudges += isMirrored(getVerticalLine(layers, j), i);
+                if (totalSmudges > smudges) continue outer;
             }
-            return i;
+            if (totalSmudges == smudges) return i;
         }
         return 0;
     }
@@ -58,16 +66,20 @@ public class Day13Solution extends Solution {
         return line.toString();
     }
 
-    private boolean isMirrored(String string, int index) {
+    /**
+     * @return the amount of differences
+     */
+    private int isMirrored(String string, int index) {
         int length = Math.min(string.length() - index, index);
-        if (length == 0) return false;
+        if (length == 0) return -1;
         String leftPart = string.substring(index - length, index);
         String rightPart = string.substring(index, index + length);
+        int diff = 0;
         for (int i = 0; i < length; i++) {
             if (leftPart.charAt(i) != rightPart.charAt(length - i - 1))
-                return false;
+                diff++;
         }
-        return true;
+        return diff;
     }
 
 }
